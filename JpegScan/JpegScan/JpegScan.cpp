5605c88e43c4b2aa3ae74c18f37270ca75452cac
Timur Safin <timur.safin@gmail.com>
2014-01-16 03:19:22 +0400

    1st JPEG Exif example compiled.

--- JpegScan/JpegScan/JpegScan.cpp
+++ JpegScan/JpegScan/JpegScan.cpp
@@ -2,10 +2,45 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
 
+#include <libexif/exif-data.h>
 
-int main(int argc, char * argv[])
+int	main (int argc, char **argv)
 {
-	return 0;
+	ExifData *d;
+	unsigned int buf_size;
+	unsigned char *buf;
+	int r;
+
+	if (argc <= 1) {
+		fprintf (stderr, "You need to supply a filename!\n");
+		return 1;
+	}
+
+	fprintf (stdout, "Loading '%s'...\n", argv[1]);
+	d = exif_data_new_from_file (argv[1]);
+	if (!d) {
+		fprintf (stderr, "Could not load data from '%s'!\n", argv[1]);
+		return 1;
+	}
+	fprintf (stdout, "Loaded '%s'.\n", argv[1]);
+
+	fprintf (stdout, "######### Test 1 #########\n");
+	//r = test_exif_data (d);
+	//if (r) return r;
+
+	exif_data_save_data (d, &buf, &buf_size);
+	exif_data_unref (d);
+	d = exif_data_new_from_data (buf, buf_size);
+	free (buf);
+
+	fprintf (stdout, "######### Test 2 #########\n");
+	//r = test_exif_data (d);
+	//if (r) return r;
+
+	fprintf (stdout, "Test successful!\n");
+
+	return 1;
 }
 
