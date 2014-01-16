a8a70c138bbb2cc097e57319e054bafce62f8941
Timur Safin <timur.safin@gmail.com>
2014-01-16 23:46:20 +0400

    Simple JPEG EXIF data dumper example. Use builtin exif_data_dump function for that.

--- JpegScan/JpegScan/JpegScan.cpp
+++ JpegScan/JpegScan/JpegScan.cpp
@@ -1,46 +1,108 @@
-// JpegScan.cpp : Defines the entry point for the console application.
-//
+/*
+ * libexif example program to display the contents of a number of specific
+ * EXIF and MakerNote tags. The tags selected are those that may aid in
+ * identification of the photographer who took the image.
+ *
+ * Placed into the public domain by Dan Fandrich
+ */
 
 #include <stdio.h>
-#include <stdlib.h>
-
+#include <string.h>
 #include <libexif/exif-data.h>
 
-int	main (int argc, char **argv)
+/* Remove spaces on the right of the string */
+static void trim_spaces(char *buf)
 {
-	ExifData *d;
-	unsigned int buf_size;
-	unsigned char *buf;
-	int r;
-
-	if (argc <= 1) {
-		fprintf (stderr, "You need to supply a filename!\n");
-		return 1;
-	}
-
-	fprintf (stdout, "Loading '%s'...\n", argv[1]);
-	d = exif_data_new_from_file (argv[1]);
-	if (!d) {
-		fprintf (stderr, "Could not load data from '%s'!\n", argv[1]);
-		return 1;
-	}
-	fprintf (stdout, "Loaded '%s'.\n", argv[1]);
-
-	fprintf (stdout, "######### Test 1 #########\n");
-	//r = test_exif_data (d);
-	//if (r) return r;
-
-	exif_data_save_data (d, &buf, &buf_size);
-	exif_data_unref (d);
-	d = exif_data_new_from_data (buf, buf_size);
-	free (buf);
-
-	fprintf (stdout, "######### Test 2 #########\n");
-	//r = test_exif_data (d);
-	//if (r) return r;
-
-	fprintf (stdout, "Test successful!\n");
-
-	return 1;
+    char *s = buf-1;
+    for (; *buf; ++buf) {
+        if (*buf != ' ')
+            s = buf;
+    }
+    *++s = 0; /* nul terminate the string on the first of the final spaces */
 }
 
+/* Show the tag name and contents if the tag exists */
+static void show_tag(ExifData *d, ExifIfd ifd, ExifTag tag)
+{
+    /* See if this tag exists */
+    ExifEntry *entry = exif_content_get_entry(d->ifd[ifd],tag);
+    if (entry) {
+        char buf[1024];
+
+        /* Get the contents of the tag in human-readable form */
+        exif_entry_get_value(entry, buf, sizeof(buf));
+
+        /* Don't bother printing it if it's entirely blank */
+        trim_spaces(buf);
+        if (*buf) {
+            printf("%s: %s\n", exif_tag_get_name_in_ifd(tag,ifd), buf);
+        }
+    }
+}
+
+/* Show the given MakerNote tag if it exists */
+static void show_mnote_tag(ExifData *d, unsigned tag)
+{
+    ExifMnoteData *mn = exif_data_get_mnote_data(d);
+    if (mn) {
+        int num = exif_mnote_data_count(mn);
+        int i;
+
+        /* Loop through all MakerNote tags, searching for the desired one */
+        for (i=0; i < num; ++i) {
+            char buf[1024];
+            if (exif_mnote_data_get_id(mn, i) == tag) {
+                if (exif_mnote_data_get_value(mn, i, buf, sizeof(buf))) {
+                    /* Don't bother printing it if it's entirely blank */
+                    trim_spaces(buf);
+                    if (*buf) {
+                        printf("%s: %s\n", exif_mnote_data_get_title(mn, i),
+                            buf);
+                    }
+                }
+            }
+        }
+    }
+}
+
+int main(int argc, char **argv)
+{
+    ExifData *ed;
+    ExifEntry *entry;
+
+    if (argc < 2) {
+        printf("Usage: %s image.jpg\n", argv[0]);
+        printf("Displays tags potentially relating to ownership "
+                "of the image.\n");
+        return 1;
+    }
+
+    /* Load an ExifData object from an EXIF file */
+    ed = exif_data_new_from_file(argv[1]);
+    if (!ed) {
+        printf("File not readable or no EXIF data in file %s\n", argv[1]);
+        return 2;
+    }
+
+#if 0
+	/* Show all the tags that might contain information about the
+     * photographer
+     */
+    show_tag(ed, EXIF_IFD_0, EXIF_TAG_ARTIST);
+    show_tag(ed, EXIF_IFD_0, EXIF_TAG_XP_AUTHOR);
+    show_tag(ed, EXIF_IFD_0, EXIF_TAG_COPYRIGHT);
+
+    /* These are much less likely to be useful */
+    show_tag(ed, EXIF_IFD_EXIF, EXIF_TAG_USER_COMMENT);
+    show_tag(ed, EXIF_IFD_0, EXIF_TAG_IMAGE_DESCRIPTION);
+    show_tag(ed, EXIF_IFD_1, EXIF_TAG_IMAGE_DESCRIPTION);
+
+#else
+	exif_data_dump(ed);
+#endif
+
+    /* Free the EXIF data */
+    exif_data_unref(ed);
+
+    return 0;
+}
