25bad10a45c5ed180252b4e03f43610f4193da41
Timur Safin <timur.safin@gmail.com>
2014-01-24 17:57:17 +0400

    Generate datestamp using exif tag values
    Shows only to the screen.. Next step is to generate BAT script commands.

--- JpegScan/JpegScan/JpegScan.cpp
+++ JpegScan/JpegScan/JpegScan.cpp
@@ -1,13 +1,6 @@
-/*
- * libexif example program to display the contents of a number of specific
- * EXIF and MakerNote tags. The tags selected are those that may aid in
- * identification of the photographer who took the image.
- *
- * Placed into the public domain by Dan Fandrich
- */
-
 #include <stdio.h>
 #include <string.h>
+#include <assert.h>
 #include <libexif/exif-data.h>
 #include <vector>
 #include <array>
@@ -18,6 +11,7 @@
 #include <ctime>
 #include "getopt.h"
 
+const char * fileNamePattern = "%04d-%02d-%02d";
 
 std::tm xlate_datetime(const std::string& s)
 {
@@ -25,6 +19,15 @@ std::tm xlate_datetime(const std::string& s)
 	std::istringstream ss(s);
 	ss >> std::get_time(&t, "%Y:%m:%d %H:%M:%S");
 	return t;
+}
+
+
+std::string format_path_tm(const std::tm & tm)
+{
+	std::array<char, 256> buffer;
+	sprintf_s(buffer.data(), buffer.size(), fileNamePattern, 
+			  tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday);
+	return buffer.data();
 }
 
 /* Remove spaces on the right of the string */
@@ -41,18 +44,16 @@ static void trim_spaces(char *buf)
 static std::string get_tag_value(ExifData *d, ExifIfd ifd, ExifTag tag) 
 {
 	std::string s_temp;
-    /* See if this tag exists */
-    ExifEntry *entry = exif_content_get_entry(d->ifd[ifd],tag);
+
+	ExifEntry *entry = exif_content_get_entry(d->ifd[ifd],tag);
     if (entry) {
 		std::array<char, 1024> buffer;
 
         /* Get the contents of the tag in human-readable form */
 		exif_entry_get_value(entry, buffer.data(), buffer.max_size());
 
-        /* Don't bother printing it if it's entirely blank */
 		trim_spaces(buffer.data());
 		size_t len = strlen(buffer.data());
-		//std::copy_n(buffer.begin(), len, s_temp.begin());
 		s_temp = buffer.data();
 	}
 	return s_temp;
@@ -64,7 +65,6 @@ static void show_tag(ExifData *d, ExifIfd ifd, ExifTag tag)
 	std::string&& buffer = get_tag_value(d, ifd, tag);
 
 	if (!buffer.empty()) {
-		std::tm&& tt = xlate_datetime(buffer);
 		printf("%s: %s\n", exif_tag_get_name_in_ifd(tag, ifd), buffer.data());
     }
 }
@@ -74,19 +74,31 @@ static char * fileName;
 static bool dumpMode;
 static bool genMode;
 
-void process_datestamp_data (ExifData *data)
+void process_datestamp_data (const char * fileName, ExifData *data)
 {
+#if 0
 	show_tag(data, EXIF_IFD_0, EXIF_TAG_DATE_TIME_DIGITIZED);
 	show_tag(data, EXIF_IFD_0, EXIF_TAG_DATE_TIME);
 	show_tag(data, EXIF_IFD_0, EXIF_TAG_DATE_TIME_ORIGINAL);
 //	show_tag(data, EXIF_IFD_GPS, (ExifTag)EXIF_TAG_GPS_DATE_STAMP);
 //	show_tag(data, EXIF_IFD_GPS, (ExifTag)EXIF_TAG_GPS_TIME_STAMP);
+#endif
+	std::string&& tag_string = get_tag_value(data, EXIF_IFD_0, EXIF_TAG_DATE_TIME_DIGITIZED);
+	if (tag_string.empty())
+		tag_string = get_tag_value(data, EXIF_IFD_0, EXIF_TAG_DATE_TIME);
+	if (tag_string.empty())
+		tag_string = get_tag_value(data, EXIF_IFD_0, EXIF_TAG_DATE_TIME_ORIGINAL);
+	assert(!tag_string.empty());
+	
+	std::tm&& file_tm = xlate_datetime(tag_string);
+	std::string&& date_string = format_path_tm(file_tm);
 
+	printf("File %s: translated = %s\n", fileName, date_string.c_str());
 }
+
 int main(int argc, char **argv)
 {
     ExifData *ed;
-    //ExifEntry *entry;
 
     if (argc < 2) {
         printf("Usage: %s image.jpg\n", argv[0]);
@@ -130,13 +142,13 @@ int main(int argc, char **argv)
 			exif_data_dump(ed);
 		}
 		else if (genMode) {
-			process_datestamp_data(ed);
+			process_datestamp_data(fileName, ed);
 		}
 		/* Free the EXIF data */
 		exif_data_unref(ed);
 
-		fileName = argv[++optind]; // process next free arguument in the command-line (wildcard expansion most-probably)
+		fileName = argv[optind++]; // process next free arguument in the command-line (wildcard expansion most-probably)
 
-	} while (optind < argc);
+	} while (optind <= argc);
     return 0;
 }
